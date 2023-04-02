aa73747aff76b53bbd6897ce5c93c6523e796368
MarcinBolt <marcin.boltruczyk@gmail.com>
2023-04-02 17:37:13 +0200

    fixed styling for mobile

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 fetch('data.json')
   .then(response => response.json())
   .then(data => {
-    const iconsDOM = document.querySelectorAll('.icon');
     const categoryDOM = document.querySelectorAll('.span-category');
     const scoreDOM = document.querySelectorAll('.span-score');
-    for (let i = 0; i < iconsDOM.length; i++) {
-      iconsDOM[i].src = data[i].icon;
+    for (let i = 0; i < categoryDOM.length; i++) {
       categoryDOM[i].textContent = data[i].category;
       scoreDOM[i].textContent = data[i].score;
     }
